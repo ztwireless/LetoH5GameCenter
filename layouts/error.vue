ea7112493b1bb3a45f69f9ae85b5d64f2e11cb31
luma <stubma@gmail.com>
2020-04-05 10:42:16 +0800

    remove text

--- layouts/error.vue
+++ layouts/error.vue
@@ -3,7 +3,7 @@
         <template v-if="error.statusCode === 404">
             <img src="~assets/img/logo.png" class="logo" />
             <h1 class="title">404</h1>
-            <p class="info">世界上最远的距离，是我装了亿刻APP，你却没有安装，<nuxt-link to="/">立即下载</nuxt-link></p>
+            <p class="info">出错啦~, 请重试</p>
         </template>
         <template v-else>
             <h1 class="title">
